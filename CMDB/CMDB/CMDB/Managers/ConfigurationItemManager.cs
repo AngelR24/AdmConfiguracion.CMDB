7cbeae85f369c9d2274aa91301abf76ddce9a887
AngelR24 <angel.ruizlajara@gmail.com>
2019-08-24 18:28:34 -0400

    Added ConfigurationItem Model and Manager

---
+++ CMDB/CMDB/CMDB/Managers/ConfigurationItemManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMDB.Managers
+{
+    public class ConfigurationItemManager
+    {
+        public void Create()
+        {
+
+        }
+
+        public void AddDependency()
+        {
+
+        }
+
+        public void ListItems()
+        {
+
+        }
+
+    }
+}
---
+++ CMDB/CMDB/CMDB/Models/ConfigurationItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMDB.Models
+{
+    public class ConfigurationItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Version { get; set; }
+        public string Responsible { get; set; }
+    }
+}
