dbb25ab55105ab70819b9e082a447b98944b4381
AngelR24 <angel.ruizlajara@gmail.com>
2019-08-24 18:36:11 -0400

    Added DataBase Annotations and Dependency Model

--- CMDB/CMDB/CMDB/Models/ConfigurationItem.cs
+++ CMDB/CMDB/CMDB/Models/ConfigurationItem.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CMDB.Models
 {
     public class ConfigurationItem
     {
+        [Key]
         public string Name { get; set; }
         public string Description { get; set; }
         public string Version { get; set; }
         public string Responsible { get; set; }
+
+        public List<Dependency> DependencyItems { get; set; }
     }
 }
---
+++ CMDB/CMDB/CMDB/Models/Dependency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CMDB.Models
+{
+    public class Dependency
+    {
+        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+
+        public string BaseCIName { get; set; }
+
+        public string DependencyCIName { get; set; }
+
+        public ConfigurationItem BaseCI { get; set; }
+
+        public ConfigurationItem DependencyCI { get; set; }
+
+    }
+}
