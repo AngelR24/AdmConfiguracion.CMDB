d45e92d56f33c1bb629ea84097f514d6b50a28bf
AngelR24 <angel.ruizlajara@gmail.com>
2019-08-24 17:55:30 -0400

    Added code to  MenuManager

--- CMDB/CMDB/CMDB/Managers/MenuManager.cs
+++ CMDB/CMDB/CMDB/Managers/MenuManager.cs
@@ -8,10 +8,19 @@ namespace CMDB.Managers
     {
         private readonly List<string> _menuItems;
         
-        public MenuManager(List<string> menuItems)
+            public MenuManager(List<string> menuItems)
         {
             _menuItems = menuItems;
         }
 
+        public void PrintMainMenu()
+        {
+            Console.WriteLine("Main menu");
+            for (int i = 0; i < _menuItems.Count; i++)
+            {
+                Console.WriteLine($"{i+1}- {_menuItems[i]}");
+            }
+            Console.WriteLine($"{_menuItems.Count + 1}- Exit");  
+        }
     }
 }
