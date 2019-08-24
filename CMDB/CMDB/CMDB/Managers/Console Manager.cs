48dcf284d54406f8b1c6b4a5cf47e70a90685f11
AngelR24 <angel.ruizlajara@gmail.com>
2019-08-24 18:16:01 -0400

    Added function dictionary to handle menu options

--- CMDB/CMDB/CMDB/Managers/Console Manager.cs
+++ CMDB/CMDB/CMDB/Managers/Console Manager.cs
@@ -12,17 +12,47 @@ namespace CMDB.Managers
             "Add new CI", //Option 1
             "Add dependency CI" //Option 2
         };
+        private readonly Dictionary<int, Action> _menuActions;
 
         public ConsoleManager()
         {
-            _menuManager = new MenuManager(_menuItems);  
+            _menuManager = new MenuManager(_menuItems);
+            _menuActions = new Dictionary<int, Action>
+            {
+                {1, AddNewCI },
+                {2, AddCIDependency }
+            };
         }
 
         public void Start()
         {
             Console.WriteLine("Application Started");
+            LoadMainScreen();
+        }
+
+        public void LoadMainScreen()
+        {
+            _menuManager.PrintMainMenu();
+            int option = _menuManager.SelectOption();
+
+            if (option == -1)
+            {
+                LoadMainScreen();
+            }
+
+            _menuActions[option]();
 
         }
 
+
+        private void AddNewCI()
+        {
+
+        }
+
+        private void AddCIDependency()
+        {
+
+        }
     }
 }
--- CMDB/CMDB/CMDB/Managers/MenuManager.cs
+++ CMDB/CMDB/CMDB/Managers/MenuManager.cs
@@ -8,7 +8,7 @@ namespace CMDB.Managers
     {
         private readonly List<string> _menuItems;
         
-            public MenuManager(List<string> menuItems)
+        public MenuManager(List<string> menuItems)
         {
             _menuItems = menuItems;
         }
@@ -21,6 +21,35 @@ namespace CMDB.Managers
                 Console.WriteLine($"{i+1}- {_menuItems[i]}");
             }
             Console.WriteLine($"{_menuItems.Count + 1}- Exit");  
+        }
+
+        public int SelectOption()
+        {
+            Console.Write("Select an option: ");
+            string answer = Console.ReadLine();
+            int option = -1;
+            bool result = Int32.TryParse(answer, out option);
+
+            if (!result)
+            {
+                InvalidInputMessage();
+                return -1;
+            }
+
+            if (option < 1 || option > _menuItems.Count + 1)
+            {
+                InvalidInputMessage();
+                return -1;
+            }
+
+            return option;
+          
+        }
+
+        public void InvalidInputMessage(string message = "Invalid input..")
+        {
+            Console.WriteLine(message);
+            Console.ReadKey();
         }
     }
 }
