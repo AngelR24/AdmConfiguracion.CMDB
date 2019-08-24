80229a4e530042437e267309e5f639cedff73e45
AngelR24 <angel.ruizlajara@gmail.com>
2019-08-24 17:32:19 -0400

    Created project

---
+++ CMDB/CMDB/CMDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CMDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
