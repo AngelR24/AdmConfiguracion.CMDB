5486a8119eafc6497a8fcca9d9ecc1bab425e4c4
AngelR24 <angel.ruizlajara@gmail.com>
2019-08-24 17:54:34 -0400

    Created MenuManager and defined inital menu items

--- CMDB/CMDB/CMDB/Managers/Console Manager.cs
+++ CMDB/CMDB/CMDB/Managers/Console Manager.cs
@@ -6,10 +6,23 @@ namespace CMDB.Managers
 {
     public class ConsoleManager
     {
+        private readonly MenuManager _menuManager;
+        private readonly List<string> _menuItems = new List<string>
+        {
+            "Add new CI", //Option 1
+            "Add dependency CI" //Option 2
+        };
+
+        public ConsoleManager()
+        {
+            _menuManager = new MenuManager(_menuItems);  
+        }
+
         public void Start()
         {
             Console.WriteLine("Application Started");
 
         }
+
     }
 }
---
+++ CMDB/CMDB/CMDB/Managers/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMDB.Managers
+{
+    public class MenuManager
+    {
+        private readonly List<string> _menuItems;
+        
+        public MenuManager(List<string> menuItems)
+        {
+            _menuItems = menuItems;
+        }
+
+    }
+}
